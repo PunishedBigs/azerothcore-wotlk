e28bf393891ae2dfa38933083a1d4a4e0f62828a
Bigs <Bigslarge@gmail.com>
2026-04-25 13:09:07 +0930

    added goblin and highelf, moved felorc

--- src/server/shared/SharedDefines.h
+++ src/server/shared/SharedDefines.h
@@ -76,10 +76,10 @@ enum Races
     RACE_TAUREN             = 6,  // TITLE Tauren
     RACE_GNOME              = 7,  // TITLE Gnome
     RACE_TROLL              = 8,  // TITLE Troll
-    //RACE_GOBLIN             = 9,
+    RACE_GOBLIN             = 9,
     RACE_BLOODELF           = 10, // TITLE Blood Elf
-    RACE_DRAENEI            = 11 //, TITLE Draenei
-    //RACE_FEL_ORC        = 12,
+    RACE_DRAENEI            = 11, //, TITLE Draenei
+    RACE_HIGHELF            = 12
     //RACE_NAGA           = 13,
     //RACE_BROKEN         = 14,
     //RACE_SKELETON       = 15,
@@ -89,6 +89,7 @@ enum Races
     //RACE_TAUNKA         = 19,
     //RACE_NORTHREND_SKELETON = 20,
     //RACE_ICE_TROLL      = 21
+    //RACE_FEL_ORC        = 22
 };
 
 // DisplayRace values from CreatureDisplayInfoExtra.dbc
@@ -106,7 +107,7 @@ enum class DisplayRace : uint8
     Goblin            = 9,
     BloodElf          = 10,
     Draenei           = 11,
-    FelOrc            = 12,
+    HighElf           = 12,
     Naga              = 13,
     Broken            = 14,
     Skeleton          = 15,
@@ -115,7 +116,8 @@ enum class DisplayRace : uint8
     ForestTroll       = 18,
     Taunka            = 19,
     NorthrendSkeleton = 20,
-    IceTroll          = 21
+    IceTroll          = 21,
+    FelOrc            = 22
 };
 
 // Class value is index in ChrClasses.dbc
