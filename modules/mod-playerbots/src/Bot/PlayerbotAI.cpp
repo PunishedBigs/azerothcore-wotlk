1a6643778bedebc7c2b5bda9bb9b4ff90e410763
Bigs <Bigslarge@gmail.com>
2026-04-25 13:51:27 +0930

    Added helf to IsAlliance

--- modules/mod-playerbots/src/Bot/PlayerbotAI.cpp
+++ modules/mod-playerbots/src/Bot/PlayerbotAI.cpp
@@ -4353,7 +4353,7 @@ bool PlayerbotAI::canDispel(SpellInfo const* spellInfo, uint32 dispelType)
 bool IsAlliance(uint8 race)
 {
     return race == RACE_HUMAN || race == RACE_DWARF || race == RACE_NIGHTELF || race == RACE_GNOME ||
-           race == RACE_DRAENEI;
+           race == RACE_DRAENEI || race == RACE_HIGHELF;
 }
 
 Player* PlayerbotAI::FindNewMaster()
